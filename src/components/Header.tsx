de35a2e13cd917cfdbb2e9ffd156862be18c4824
itona <naov7700@gmail.com>
2023-02-15 21:34:18 +0900

    Add actions for each button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,31 @@
-import { Box, Button, Flex, Link, Stack } from "@chakra-ui/react";
+/* eslint-disable react/jsx-no-undef */
+import {
+  Box,
+  Button,
+  Flex,
+  Link,
+  Stack,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import React from "react";
 
 import { AccountControlButton } from "./AccountControlButton";
 
 export const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
   return (
     <Box w="100%" h="80px" bg="purple.300">
       <Flex
@@ -13,31 +35,87 @@ export const Header = () => {
         alignItems="center"
       >
         <Link href="/">
-          <Button colorScheme="black" variant="outline" ml="10">
+          <Button
+            colorScheme="whiteAlpha"
+            variant="outline"
+            ml="10"
+            color="purple"
+            w="130px"
+          >
             トップへ戻る
           </Button>
         </Link>
         <Stack direction="row" spacing={4} mr="10">
-          <AccountControlButton
+          {/* <AccountControlButton
             text="ログイン"
             colorScheme="whiteAlpha"
             color="purple"
-          />
-          <AccountControlButton
-            text="ログアウト"
-            colorScheme="whiteAlpha"
+            width="130px"
+            /> */}
+          <Button
+            onClick={onOpen}
             color="purple"
-          />
-          <AccountControlButton
-            text="マイページ"
-            colorScheme="purple"
-            color="white"
-          />
+            colorScheme="whiteAlpha"
+            w="130px"
+          >
+            ログイン
+          </Button>
+
+          <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>アカウント情報を入力してください</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>メールアドレス</FormLabel>
+                  <Input ref={initialRef} placeholder="*******@email.com" />
+                </FormControl>
+
+                <FormControl mt={4}>
+                  <FormLabel>パスワード</FormLabel>
+                  <Input placeholder="password" />
+                </FormControl>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3}>
+                  Save
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+
+          <Link href="/">
+            <AccountControlButton
+              text="ログアウト"
+              colorScheme="whiteAlpha"
+              color="purple"
+              width="130px"
+            />
+          </Link>
+          <Link href="/myPage">
+            <AccountControlButton
+              text="マイページ"
+              colorScheme="purple"
+              color="white"
+              width="130px"
+            />
+          </Link>
+          <Link href="newAccount">
           <AccountControlButton
             text="新規登録(無料)"
             colorScheme="purple"
             color="white"
-          />
+            width="130px"
+            />
+            </Link>
         </Stack>
       </Flex>
     </Box>
---
+++ src/pages/newAccount/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const newAccount = () => {
+  return <div>newAccount</div>;
+};
+
+export default newAccount;
