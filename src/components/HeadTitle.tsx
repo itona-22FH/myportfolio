04e92b246c63549ab11829ee146e9562d29b41cf
itona <naov7700@gmail.com>
2023-02-19 15:54:52 +0900

    Componentization of the title part

---
+++ src/components/HeadTitle.tsx
@@ -0,0 +1,15 @@
+import { Box, Divider, Heading } from "@chakra-ui/react";
+import React from "react";
+
+export const HeadTitle = ({ title }: HeadTitleProps) => {
+  return (
+    <Box>
+      <Heading as="h2" size="2xl" pt="20px">
+        {title}
+      </Heading>
+      <Divider borderColor="purple.300" pt="10px" />
+    </Box>
+  );
+};
+
+
