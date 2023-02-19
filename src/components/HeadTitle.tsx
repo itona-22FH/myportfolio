3d8140994a02754c4a3a14d88347a5b24793a283
itona <naov7700@gmail.com>
2023-02-19 20:33:02 +0900

    CSS fixes

--- src/components/HeadTitle.tsx
+++ src/components/HeadTitle.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export const HeadTitle = ({ title }: HeadTitleProps) => {
   return (
     <Box>
-      <Heading as="h2" size="2xl" pt="20px">
+      <Heading as="h2" size="xl" pt="20px">
         {title}
       </Heading>
       <Divider borderColor="purple.300" pt="10px" />
