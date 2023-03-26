d7ce911347b45a70099b33242d3170b6955a4f98
itona <naov7700@gmail.com>
2023-03-26 16:15:36 +0900

    ログイン中のユーザー情報のみを表示

--- src/pages/myPage/index.tsx
+++ src/pages/myPage/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import {
   Avatar,
   Box,
@@ -13,112 +14,128 @@ import {
   StackDivider,
 } from "@chakra-ui/react";
 import React from "react";
+import { useRecoilValue } from "recoil";
 import { AccountControlButton } from "../../components/AccountControlButton";
+import { profileCollectionAtom } from "../../components/atoms/profileCollectionAtom";
+import { testLoginUserAtom } from "../../components/atoms/testloginUserAtom";
 import { GamePlan } from "../../components/GamePlan";
 import { TextBox } from "../../components/TextBox";
 
 const myPage = () => {
-  const demoText =
-    " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua.DConsequat nisl vel pretium lectus quam id. Semper quis lectusnulla at volutpat diam ut venenatis. Dolor morbi non arcu risusquis varius quam quisque. Massa ultricies mi quis hendrerit dolor  magna eget est lorem. Erat imperdiet sed euismod nisi portaLectus vestibulum mattis ullamcorper velit.";
+  const profileCollections = useRecoilValue(profileCollectionAtom);
+  const testLoginUser = useRecoilValue(testLoginUserAtom);
   return (
-    <Box pt="10px" pb="10px">
-      <Container maxW="1100px">
-        <Flex
-          alignItems="center"
-          bg="whiteAlpha.800"
-          borderRadius="10px"
-          p="10px"
-        >
-          <Avatar
-            size="2xl"
-            name="Segun Adebayo"
-            src="https://bit.ly/dan-abramov"
-          />
-          <Text mt="10px" fontSize="35" ml="10px" fontWeight="bold">
-            Hello User
-          </Text>
-        </Flex>
-
-        <Flex
-          justifyContent="space-around"
-          bg="whiteAlpha.800"
-          mt="10px"
-          p="10px"
-          borderRadius="10px"
-          w="100%"
-        >
-          <Box>
-            <AccountControlButton
-              text="プロフィールを編集する"
-              colorScheme="purple"
-              color="white"
-              width="400px"
-              href="/editProfile"
-            />
-          </Box>
-          <Box>
-            <AccountControlButton
-              text="新規プランを登録する"
-              colorScheme="purple"
-              color="white"
-              width="400px"
-              href="/newPlan"
-            />
-          </Box>
-        </Flex>
-
-        <Box
-          h="100%"
-          bg="whiteAlpha.800"
-          mt="10px"
-          borderRadius="10px"
-          p="10px"
-        >
-          <Tabs colorScheme="purple">
-            <TabList>
-              <Tab>プロフィール</Tab>
-              <Tab>レビュー</Tab>
-              <Tab>登録プラン</Tab>
-              <Tab>契約中のプラン</Tab>
-            </TabList>
-
-            <TabPanels>
-              <TabPanel>
-                <VStack
-                  divider={<StackDivider borderColor="purple" />}
-                  spacing={2}
-                  align="top"
-                >
-                  <TextBox title="自己紹介" text={demoText} fontSize="20px" />
-                  <TextBox title="経歴・実績" text={demoText} fontSize="20px" />
-                </VStack>
-              </TabPanel>
-              <TabPanel></TabPanel>
-              <TabPanel>
+    <>
+      {profileCollections.map(
+        ({
+          userID,
+          userName,
+          userAvatar,
+          email,
+          password,
+          twitterAccount,
+          youtubeAccount,
+          selfIntroduction,
+          achievement,
+          reviewCount,
+          reviewScore,
+        }) => {
+          if (userID === testLoginUser) {
+            <Box pt="10px" pb="10px">
+              <Container maxW="1100px">
                 <Flex
-                  align="center"
-                  wrap="wrap"
-                  maxW="1000px"
+                  alignItems="center"
+                  bg="whiteAlpha.800"
                   borderRadius="10px"
+                  p="10px"
                 >
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
-                  <GamePlan />
+                  <Avatar size="2xl" name="Segun Adebayo" src={userAvatar} />
+                  <Text mt="10px" fontSize="35" ml="10px" fontWeight="bold">
+                    {userName}
+                  </Text>
                 </Flex>
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </Box>
-      </Container>
-    </Box>
+
+                <Flex
+                  justifyContent="space-around"
+                  bg="whiteAlpha.800"
+                  mt="10px"
+                  p="10px"
+                  borderRadius="10px"
+                  w="100%"
+                >
+                  <Box>
+                    <AccountControlButton
+                      text="プロフィールを編集する"
+                      colorScheme="purple"
+                      color="white"
+                      width="400px"
+                      href="/editProfile"
+                    />
+                  </Box>
+                  <Box>
+                    <AccountControlButton
+                      text="新規プランを登録する"
+                      colorScheme="purple"
+                      color="white"
+                      width="400px"
+                      href="/newPlan"
+                    />
+                  </Box>
+                </Flex>
+
+                <Box
+                  h="100%"
+                  bg="whiteAlpha.800"
+                  mt="10px"
+                  borderRadius="10px"
+                  p="10px"
+                >
+                  <Tabs colorScheme="purple">
+                    <TabList>
+                      <Tab>プロフィール</Tab>
+                      <Tab>登録中のプラン</Tab>
+                      <Tab>契約プラン履歴</Tab>
+                    </TabList>
+
+                    <TabPanels>
+                      <TabPanel>
+                        <VStack
+                          divider={<StackDivider borderColor="purple" />}
+                          spacing={2}
+                          align="top"
+                        >
+                          <TextBox
+                            title="自己紹介"
+                            text={selfIntroduction}
+                            fontSize="20px"
+                          />
+                          <TextBox
+                            title="経歴・実績"
+                            text={achievement}
+                            fontSize="20px"
+                          />
+                        </VStack>
+                      </TabPanel>
+                      <TabPanel>
+                        <Flex
+                          align="center"
+                          wrap="wrap"
+                          maxW="1000px"
+                          borderRadius="10px"
+                        >
+                          <GamePlan />
+                        </Flex>
+                      </TabPanel>
+                      <TabPanel></TabPanel>
+                    </TabPanels>
+                  </Tabs>
+                </Box>
+              </Container>
+            </Box>
+          }
+        }
+      )}
+    </>
   );
 };
 
