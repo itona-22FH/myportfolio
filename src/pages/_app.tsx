0c2b6b988b83d9bf7d7d64f073eb66b46294c17b
itona <naov7700@gmail.com>
2023-04-02 21:35:41 +0900

    import追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Layout } from "../layout/Layout";
 import { RecoilRoot } from "recoil";
+import React from "react";
+import { Route } from "react-router-dom";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
