b8b18e64223f2367314b93957d798b20019f93a5
itona <naov7700@gmail.com>
2023-04-04 22:45:31 +0900

    不要なImport文の削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { Layout } from "../layout/Layout";
 import { RecoilRoot } from "recoil";
 import React from "react";
-import { Route } from "react-router-dom";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
