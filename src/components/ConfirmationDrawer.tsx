2ba8614d60017afb6de92c2512cc7f0426461a4c
itona <naov7700@gmail.com>
2023-04-06 21:35:44 +0900

    Drawerをコンポーネント化

---
+++ src/components/ConfirmationDrawer.tsx
@@ -0,0 +1,74 @@
+import {
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  VStack,
+  StackDivider,
+  Flex,
+  Heading,
+  useDisclosure,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+import React from "react";
+import { ConfirmationBtn } from "./ConfirmationBtn";
+import { TextBox } from "./TextBox";
+
+export const ConfirmationDrawer = (plan: Plan) => {
+  const { isOpen, onClose } = useDisclosure();
+  return (
+    <Drawer onClose={onClose} isOpen={isOpen} size="full">
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader fontSize="40px" color="purple" fontWeight="bold">
+          プラン契約確認画面
+        </DrawerHeader>
+        <DrawerBody>
+          <VStack
+            divider={<StackDivider borderColor="purple" />}
+            spacing="4px"
+            align="stretch"
+            fontWeight="bold"
+          >
+            <TextBox
+              title="プラン名"
+              text={plan.planTitle}
+              fontSize="20px"
+            />
+            <TextBox
+              title="メンター名"
+              text={plan.userName}
+              fontSize="20px"
+            />
+            <Flex
+              justifyContent="space-between"
+              mt="20px"
+              mb="20px"
+              alignItems="center"
+            >
+              <Heading as="h3" size="lg">
+                プラン料金
+              </Heading>
+              <Text ml="30px" fontSize="40px">
+                <span style={{ color: "red" }}>{plan.price}</span>円
+              </Text>
+            </Flex>
+          </VStack>
+        </DrawerBody>
+        <Box p="10px">
+          <ConfirmationBtn
+            text="プランを契約する"
+            colorScheme="purple"
+            color="white"
+            width="100%"
+            confirmation="契約"
+          />
+        </Box>
+      </DrawerContent>
+    </Drawer>
+  );
+};
