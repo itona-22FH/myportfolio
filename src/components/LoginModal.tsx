b40aae1defc706ad017c38b26b879ad97120f7d6
itona <naov7700@gmail.com>
2023-04-06 22:21:00 +0900

    ログイン用モーダルのコンポーネント化

---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,72 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+
+export const LoginModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        color="purple"
+        colorScheme="whiteAlpha"
+        w="130px"
+      >
+        ログイン
+      </Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        size="3xl"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>アカウント情報を入力してください</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb="6px">
+            <FormControl>
+              <FormLabel>メールアドレス</FormLabel>
+              <Input
+                type="email"
+                ref={initialRef}
+                placeholder="*******@email.com"
+                name="email"
+              />
+            </FormControl>
+
+            <FormControl mt="4px">
+              <FormLabel>パスワード</FormLabel>
+              <Input type="password" placeholder="password" name="password" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr="10px">
+              ログイン
+            </Button>
+            <Button onClick={onClose} colorScheme="red">
+              キャンセル
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
