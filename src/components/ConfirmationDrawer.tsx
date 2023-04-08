1f34493ca846b41238a62cb197512bcf94c1d7f2
itona <naov7700@gmail.com>
2023-04-08 12:27:39 +0900

    インデント修正とプロパティの型の定義

--- src/components/ConfirmationDrawer.tsx
+++ src/components/ConfirmationDrawer.tsx
@@ -18,68 +18,67 @@ import { ConfirmationBtn } from "./ConfirmationBtn";
 import ContractBtn from "./ContractBtn";
 import { TextBox } from "./TextBox";
 
-export const ConfirmationDrawer = (plan: Plan) => {
-  const { isOpen, onOpen ,onClose } = useDisclosure();
+export const ConfirmationDrawer = ({ planData }: ConfirmationDrawerProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-    <ContractBtn
-                      text="プラン契約に進む"
-                      colorScheme="purple"
-                      color="white"
-                      width="100%"
-                      onClick={onOpen}
-                    />
-    <Drawer onClose={onClose} isOpen={isOpen} size="full">
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerHeader fontSize="40px" color="purple" fontWeight="bold">
-          プラン契約確認画面
-        </DrawerHeader>
-        <DrawerBody>
-          <VStack
-            divider={<StackDivider borderColor="purple" />}
-            spacing="4px"
-            align="stretch"
-            fontWeight="bold"
-          >
-            <TextBox
-              title="プラン名"
-              text={plan.planTitle}
-              fontSize="20px"
-            />
-            <TextBox
-              title="メンター名"
-              text={plan.userName}
-              fontSize="20px"
-            />
-            <Flex
-              justifyContent="space-between"
-              mt="20px"
-              mb="20px"
-              alignItems="center"
+      <ContractBtn
+        text="プラン契約に進む"
+        colorScheme="purple"
+        color="white"
+        width="100%"
+        onClick={onOpen}
+      />
+      <Drawer onClose={onClose} isOpen={isOpen} size="full">
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader fontSize="40px" color="purple" fontWeight="bold">
+            プラン契約確認画面
+          </DrawerHeader>
+          <DrawerBody>
+            <VStack
+              divider={<StackDivider borderColor="purple" />}
+              spacing="4px"
+              align="stretch"
+              fontWeight="bold"
             >
-              <Heading as="h3" size="lg">
-                プラン料金
-              </Heading>
-              <Text ml="30px" fontSize="40px">
-                <span style={{ color: "red" }}>{plan.price}</span>円
-              </Text>
-            </Flex>
-          </VStack>
-        </DrawerBody>
-        <Box p="10px">
-          <ConfirmationBtn
-            text="プランを契約する"
-            colorScheme="purple"
-            color="white"
-            width="100%"
-            confirmation="契約"
-          />
-        </Box>
-      </DrawerContent>
-    </Drawer>
+              <TextBox
+                title="プラン名"
+                text={planData.planTitle}
+                fontSize="20px"
+              />
+              <TextBox
+                title="メンター名"
+                text={planData.userName}
+                fontSize="20px"
+              />
+              <Flex
+                justifyContent="space-between"
+                mt="20px"
+                mb="20px"
+                alignItems="center"
+              >
+                <Heading as="h3" size="lg">
+                  プラン料金
+                </Heading>
+                <Text ml="30px" fontSize="40px">
+                  <span style={{ color: "red" }}>{planData.price}</span>円
+                </Text>
+              </Flex>
+            </VStack>
+          </DrawerBody>
+          <Box p="10px">
+            <ConfirmationBtn
+              text="プランを契約する"
+              colorScheme="purple"
+              color="white"
+              width="100%"
+              confirmation="契約"
+            />
+          </Box>
+        </DrawerContent>
+      </Drawer>
     </>
-
   );
 };
