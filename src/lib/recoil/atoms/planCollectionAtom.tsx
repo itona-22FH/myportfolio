0b8698be44653f8acf01fc3196fa59233783704d
itona <naov7700@gmail.com>
2023-04-18 22:15:43 +0900

    仮データの修正

--- src/lib/recoil/atoms/planCollectionAtom.tsx
+++ src/lib/recoil/atoms/planCollectionAtom.tsx
@@ -12,19 +12,23 @@ export const planCollectionAtom = atom<Plan[]>({
       //プラン登録者（FirebaseID）
       userID: "userA",
       //プラン名
-      planTitle: "AAAAAAA",
+      planTitle: "APEX",
       //プランサムネイル
       planImage: "https://bit.ly/2Z4KKcF",
       //学べる内容
-      study: "AAAAAAA",
+      study: "エーペックスレジェンズについて学べます",
       //指導方法
-      guidance: "AAAA",
+      guidance: "画面共有ツールを使用します",
+      //ゲームタイトル
+      titleCategory:"エーペックスレジェンズ",
+      //ゲームジャンル
+      genreCategory:"FPS・TPS",
       //料金
       price: "2,000",
       //アバター画像
       userAvatar: "https://bit.ly/dan-abramov",
       //ユーザー名
-      userName: "AAAAAAAAA",
+      userName: "スティーブ",
       //レビュー数
       reviewCount: 10,
       //レビュースコア
@@ -33,39 +37,45 @@ export const planCollectionAtom = atom<Plan[]>({
     {
       planID: "BplanID1",
       userID: "userB",
-      planTitle: "BBBBBBB",
+      planTitle: "FIFA",
       planImage: "https://bit.ly/2Z4KKcF",
-      study: "BBBBBBB",
-      guidance: "BBBBBBBBBB",
+      study: "FIFAについて学べます",
+      guidance: "Discordを使います",
+      titleCategory:" FIFA",
+      genreCategory:"スポーツ",
       price: "1,500",
       userAvatar: "https://bit.ly/code-beast",
-      userName: "BBBBBBBBB",
+      userName: "マイク",
       reviewCount: 120,
       reviewScore: 400,
     },
     {
       planID: "CplanID1",
       userID: "userC",
-      planTitle: "CCCCCCC",
+      planTitle: "ストリートファイター",
       planImage: "https://bit.ly/2Z4KKcF",
-      study: "CCCCCCC",
-      guidance: "CCCCCCCCCC",
+      study: "ストリートファイターについて学べます",
+      guidance: "ZOOMを使います",
+      titleCategory:"ストリートファイター",
+      genreCategory:"格闘",
       price: "2,000",
       userAvatar: "https://bit.ly/ryan-florence",
-      userName: "CCCCCCCCC",
+      userName: "マイケル",
       reviewCount: 400,
       reviewScore: 1000,
     },
     {
       planID: "DplanID1",
       userID: "userD",
-      planTitle: "DDDDDDD",
+      planTitle: "リーグオブレジェンド",
       planImage: "https://bit.ly/2Z4KKcF",
-      study: "DDDDDDDDDD",
-      guidance: "DDDDDDDDDD",
+      study: "リーグ・オブ・レジェンについて学べます",
+      guidance: "コーチング動画を渡します",
+      titleCategory:" リーグ・オブ・レジェンド",
+      genreCategory:"MOBA",
       price: "1,000",
       userAvatar: "https://bit.ly/kent-c-dodds",
-      userName: "DDDDDDDDD",
+      userName: "ジョン",
       reviewCount: 150,
       reviewScore: 800,
     },
