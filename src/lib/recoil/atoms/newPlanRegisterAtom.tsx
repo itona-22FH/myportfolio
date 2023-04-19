b059d90c748740d5b77f77f2b69c98e29750fd86
itona <naov7700@gmail.com>
2023-04-19 23:17:53 +0900

    STATEの修正

--- src/lib/recoil/atoms/newPlanRegisterAtom.tsx
+++ src/lib/recoil/atoms/newPlanRegisterAtom.tsx
@@ -1,23 +1,9 @@
 import React from "react";
 import { atom } from "recoil";
 
-const newPlanRegisterAtom = atom<Plan>({
+const newPlanRegisterAtom = atom({
   key: "newPlanRegisterAtom",
-  default: {
-    planID: "",
-    userID: "",
-    planTitle: "",
-    planImage: "",
-    study: "",
-    guidance: "",
-    titleCategory: "",
-    genreCategory: "",
-    price: "",
-    userAvatar: "",
-    userName: "",
-    reviewCount: 0,
-    reviewScore: 0,
-  },
+  default: {},
 });
 
 export default newPlanRegisterAtom;
