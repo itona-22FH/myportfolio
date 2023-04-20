94ca7ee9e04d6ab219e3c5835f67bd1f96c72317
itona <naov7700@gmail.com>
2023-04-20 23:29:40 +0900

    重複データの削除

--- src/lib/recoil/atoms/planCollectionAtom.tsx
+++ src/lib/recoil/atoms/planCollectionAtom.tsx
@@ -26,13 +26,6 @@ export const planCollectionAtom = atom<Plan[]>({
       //料金
       price: "2,000",
       //アバター画像
-      userAvatar: "https://bit.ly/dan-abramov",
-      //ユーザー名
-      userName: "スティーブ",
-      //レビュー数
-      reviewCount: 10,
-      //レビュースコア
-      reviewScore: 38,
     },
     {
       planID: "BplanID1",
@@ -44,10 +37,6 @@ export const planCollectionAtom = atom<Plan[]>({
       titleCategory:" FIFA",
       genreCategory:"スポーツ",
       price: "1,500",
-      userAvatar: "https://bit.ly/code-beast",
-      userName: "マイク",
-      reviewCount: 120,
-      reviewScore: 400,
     },
     {
       planID: "CplanID1",
@@ -59,10 +48,6 @@ export const planCollectionAtom = atom<Plan[]>({
       titleCategory:"ストリートファイター",
       genreCategory:"格闘",
       price: "2,000",
-      userAvatar: "https://bit.ly/ryan-florence",
-      userName: "マイケル",
-      reviewCount: 400,
-      reviewScore: 1000,
     },
     {
       planID: "DplanID1",
@@ -74,10 +59,6 @@ export const planCollectionAtom = atom<Plan[]>({
       titleCategory:" リーグ・オブ・レジェンド",
       genreCategory:"MOBA",
       price: "1,000",
-      userAvatar: "https://bit.ly/kent-c-dodds",
-      userName: "ジョン",
-      reviewCount: 150,
-      reviewScore: 800,
     },
   ],
   effects_UNSTABLE: [persistAtom],
