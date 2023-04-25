a77f8d65787bd410e738dc0cd8a0782aa1767a26
itona <naov7700@gmail.com>
2023-04-25 22:06:02 +0900

    新規アカウント登録機能の追加

--- src/pages/newAccount/index.tsx
+++ src/pages/newAccount/index.tsx
@@ -1,12 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
 import { Box, FormControl, Container } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ConfirmationBtn } from "../../components/ConfirmationBtn";
 import { FormInput } from "../../components/FormInput";
 import { HeadTitle } from "../../components/HeadTitle";
 import { NewRegisterTextBox } from "../../components/NewRegisterTextBox";
 import { v4 as uuidv4 } from "uuid";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import newAccountRegisterAtom from "../../lib/recoil/atoms/newAccountRegisterAtom";
+import { profileCollectionAtom } from "../../lib/recoil/atoms/profileCollectionAtom";
 
 const newAccount = () => {
+  const [newUserData, setNewUserData] = useRecoilState(newAccountRegisterAtom);
+  const [checkPassword, setCheckPassword] = useState("");
+  const setProfileCollections = useSetRecoilState(profileCollectionAtom);
+
+  useEffect(() => {
+    setNewUserData((prev) => ({ ...prev, userID: uuidv4() }));
+  }, []);
+
+  const inputUserInformation = (e: {
+    target: { name: string; value: string | number };
+  }) => {
+    const { name, value } = e.target;
+    setNewUserData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const addNewAccountHandle = (e: { preventDefault: () => void }) => {
+    if (newUserData.password === checkPassword) {
+      setProfileCollections((prev) => [...prev, newUserData]);
+    } else {
+      console.log("エラー");
+    }
+  };
+
   return (
     <Box pt="10px" pb="10px">
       <Container maxW="1100px" bg="whiteAlpha.800" p="5px" borderRadius="10px">
@@ -17,62 +45,67 @@ const newAccount = () => {
             type="text"
             placeholder="userName"
             formName="userName"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <FormInput
             label="プロフィール画像"
             type="file"
             placeholder="画像を選択してください"
             formName="userAvatar"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <FormInput
             label="メールアドレス"
             type="email"
             placeholder="********@email.com"
             formName="email"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <FormInput
             label="パスワード"
             type="password"
             placeholder="password"
             formName="password"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <FormInput
             label="確認用パスワード"
             type="password"
             placeholder="CheckPassword"
             formName="checkPassword"
-            onChangeHandle={undefined}
+            onChangeHandle={(e: {
+              target: { value: React.SetStateAction<string> };
+            }) => {
+              setCheckPassword(e.target.value);
+              console.log(checkPassword);
+            }}
           />
           <FormInput
             label="Twitterアカウント"
             type="url"
             placeholder="https://twitter.com/..."
             formName="twitterAccount"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <FormInput
             label="Youtubeアカウント"
             type="url"
             placeholder="https://www.youtube.com.channel/..."
             formName="youtubeAccount"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <NewRegisterTextBox
             htmlFor="自己紹介"
             placeholder="こんにちは、〇〇クラン所属のHelloUserです！！"
             textBoxName="selfIntroduction"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
           <NewRegisterTextBox
             htmlFor="経歴・実績"
             placeholder="〇〇大会優勝
             〇〇大会BEST3"
             textBoxName="achievement"
-            onChangeHandle={undefined}
+            onChangeHandle={inputUserInformation}
           />
         </FormControl>
         <ConfirmationBtn
@@ -81,8 +114,8 @@ const newAccount = () => {
           color="white"
           width="100%"
           confirmation="新規登録"
-          handleConfirmation={() => {}}
-          confirmationLink={"/"}
+          handleConfirmation={addNewAccountHandle}
+          confirmationLink={"/newAccount"}
         />
       </Container>
     </Box>
