2cb70ce0a3f55e70aa60205ad99a9bd9a41c797a
itona <naov7700@gmail.com>
2023-04-29 22:04:08 +0900

    類似しているファイル名変更

---
+++ src/lib/recoil/atoms/accountInformationAtom.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { atom } from "recoil";
+
+//新規アカウントの初期値
+const accountInformationAtom = atom<User>({
+  key: "accountInformationAtom",
+  default: {
+    userID: "",
+    userName: "",
+    userAvatar: "",
+    email: "",
+    password: "",
+    twitterAccount: "",
+    youtubeAccount: "",
+    selfIntroduction: "",
+    achievement: "",
+    review: [],
+  },
+});
+
+export default accountInformationAtom;
