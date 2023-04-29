9cd52b0822776f95b82bd8f3a1785e83a88cf790
itona <naov7700@gmail.com>
2023-04-29 15:42:39 +0900

    マイページボタンにアバター表示の追加

--- src/components/AccountControlButton.tsx
+++ src/components/AccountControlButton.tsx
@@ -1,5 +1,8 @@
-import { Button, Link } from "@chakra-ui/react";
+import { Avatar, Button, Link } from "@chakra-ui/react";
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { profileCollectionAtom } from "../lib/recoil/atoms/profileCollectionAtom";
+import { testLoginUserAtom } from "../lib/recoil/atoms/testLoginUserAtom";
 
 export const AccountControlButton = ({
   text,
@@ -8,9 +11,27 @@ export const AccountControlButton = ({
   width,
   href,
 }: AccountControlBtnProps) => {
+  const loginUser = useRecoilValue(testLoginUserAtom);
+  const profileCollections = useRecoilValue(profileCollectionAtom);
+
+  const userData = profileCollections.find((profile) => {
+    if (loginUser === profile.userID) {
+      return profile;
+    }
+  });
+
   return (
     <Link href={href} style={{ textDecoration: "none" }}>
       <Button colorScheme={colorScheme} variant="solid" color={color} w={width}>
+        {text === "マイページ" ? (
+          <Avatar
+            size="xs"
+            name={userData?.userName}
+            src={userData?.userAvatar}
+          />
+        ) : (
+          ""
+        )}
         {text}
       </Button>
     </Link>
