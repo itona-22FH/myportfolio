747d50f075783de5ac0b28a8268e9cdbdf34e2bc
itona <naov7700@gmail.com>
2023-04-30 14:19:47 +0900

    レビュー情報表示の修正

--- src/components/ReviewStatus.tsx
+++ src/components/ReviewStatus.tsx
@@ -1,35 +1,35 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Box } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import ReactStarsRating from "react-awesome-stars-rating";
+import ReactStars from "react-stars";
 
 export const ReviewStatus = ({ review }: ReviewStatusProps) => {
   //レビュー数のカウント
-  const reviewCount = review?.length;
-  //レビュースコア用配列
-  const reviewScoreArray: number[] = [];
+  const reviewCount = Object.keys(review).length;
 
-
-  //レビュースコアをreviewScoreArrayへ格納
-  review?.forEach((reviewScore: number) => {
-    reviewScoreArray.push(...Object.values(reviewScore));
-  });
-
-  //reviewScoreArrayをすべて合計した値を代入
-  const totalReviewScore = reviewScoreArray.reduce(
+  //rすべて合計した値を代入
+  const sumReviewScore = Object.values(review).reduce(
     (accumulator: number, currentValue: number) => accumulator + currentValue,
     0
   );
 
   //レビュースコアの平均値を算出
   const averageReviewScore =
-    Math.floor((totalReviewScore / reviewCount) * 10) / 10;
+    Math.floor((sumReviewScore / reviewCount) * 10) / 10;
 
   return (
     <Box display="flex" mt="2px" mb="2px" alignItems="center">
       {averageReviewScore ? averageReviewScore : 0}
-      <ReactStarsRating value={totalReviewScore / reviewCount} isEdit={false} />
+      <ReactStars
+        count={5}
+        size={24}
+        color2={"#ffa500"}
+        color1={"#696969"}
+        edit={false}
+        value={averageReviewScore}
+      />
       <Box as="span" ml="2" color="gray" fontSize="sm">
         {reviewCount} reviews
       </Box>
