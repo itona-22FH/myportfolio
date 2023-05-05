5b8e40aa4b590ea58291e19d4d9a2e1717beadff
itona <naov7700@gmail.com>
2023-05-05 20:32:23 +0900

    localStorage連携

--- src/lib/recoil/atoms/planCollectionAtom.tsx
+++ src/lib/recoil/atoms/planCollectionAtom.tsx
@@ -1,10 +1,10 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
-// const { persistAtom } = recoilPersist();
+const { persistAtom } = recoilPersist();
 
 export const planCollectionAtom = atom<Plan[]>({
   key: "planCollectionAtom",
   default: [],
-  // effects_UNSTABLE: [persistAtom],
+  effects_UNSTABLE: [persistAtom],
 });
--- src/lib/recoil/atoms/profileCollectionAtom.tsx
+++ src/lib/recoil/atoms/profileCollectionAtom.tsx
@@ -1,10 +1,10 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
-// const { persistAtom } = recoilPersist();
+const { persistAtom } = recoilPersist();
 
 export const profileCollectionAtom = atom<User[]>({
   key: "profileCollectionAtom",
   default: [],
-  // effects_UNSTABLE: [persistAtom],
+  effects_UNSTABLE: [persistAtom],
 });
